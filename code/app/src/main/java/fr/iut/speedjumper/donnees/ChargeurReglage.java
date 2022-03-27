417d51a45d1d79e3a64172eb8c72a538e552ab6d
gutoumazet <Guillaume.TOUMAZET@etu.uca.fr>
2022-03-27 22:19:52 +0200

    Modification de diagramme de classes + ajout de commentaire

--- code/app/src/main/java/fr/iut/speedjumper/donnees/ChargeurReglage.java
+++ code/app/src/main/java/fr/iut/speedjumper/donnees/ChargeurReglage.java
@@ -8,7 +8,19 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
+/**
+ * Classe permettant de charger depuis un fichier les reglages
+ */
+
 public class ChargeurReglage implements ChargeurReglageInterface {
+
+    /**
+     * methode permettant de charger les reglage depuis le fichier fourni en entre
+     * @param fichier
+     * @return un serializable avec les deux valeurs pour le reglages
+     * @throws FileNotFoundException
+     * @throws ClassNotFoundException
+     */
     @Nullable
     @Override
     public Serializable load(FileInputStream fichier){
--- code/app/src/main/java/fr/iut/speedjumper/donnees/ChargeurReglageInterface.java
+++ code/app/src/main/java/fr/iut/speedjumper/donnees/ChargeurReglageInterface.java
@@ -2,6 +2,10 @@ package fr.iut.speedjumper.donnees;
 
 import java.io.FileInputStream;
 
+/**
+ * interface pour charger les reglages
+ */
+
 public interface ChargeurReglageInterface {
 
     Object load(FileInputStream fichier);
